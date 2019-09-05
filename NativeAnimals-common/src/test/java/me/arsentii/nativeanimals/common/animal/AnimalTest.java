bacbd8fee8bfa3c508ecf0de471dc011b9c01af2
Zakharchenko Arsentii <arsentii.zakharchenko@gmail.com>
2019-09-05 20:00:23 +1000

    added test for animal model

---
+++ NativeAnimals-common/src/test/java/me/arsentii/nativeanimals/common/animal/AnimalTest.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package me.arsentii.nativeanimals.common.animal;
+
+/**
+ *
+ * @author zakhar
+ */
+public class AnimalTest {
+    
+}
