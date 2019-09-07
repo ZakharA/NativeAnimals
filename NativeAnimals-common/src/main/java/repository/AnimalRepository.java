b53f69d0a3e7ea3c31a848da8e9fe8c67d130008
Zakharchenko Arsentii <arsentii.zakharchenko@gmail.com>
2019-09-07 15:01:52 +1000

    add basic methods in animal repository interface

--- NativeAnimals-common/src/main/java/repository/AnimalRepository.java
+++ NativeAnimals-common/src/main/java/repository/AnimalRepository.java
@@ -6,10 +6,38 @@
 
 package repository;
 
+import java.util.List;
+import javax.ejb.Remote;
+import me.arsentii.nativeanimals.common.entities.Animal;
+
 /**
  *
  * @author zakhar
  */
+@Remote
 public interface AnimalRepository {
-    
+
+    /** add a new animal into the repository
+     *
+     * @param animal
+     */
+    public void addNewAnimal(Animal animal);
+
+    /** remove animal from the repository by animal id
+     *
+     * @param animalId
+     */
+    public void removeAnimal(long animalId);
+
+    /** merge update animal instance with persisted one in database
+     *
+     * @param animal
+     */
+    public void modifyAnimal(Animal animal);
+
+    /** select all animals from the repository
+     *
+     * @return a list of all animals 
+     */
+    public List<Animal> getAllAnimals();
 }
